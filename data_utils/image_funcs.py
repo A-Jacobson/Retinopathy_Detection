94385a7a3d8dc64e74525e6f1be3f69de80df9f9
Austin <jacobsonaustinj@gmail.com>
2016-08-20 01:46:24 -0400

    get and resizing working

--- data_utils/image_funcs.py
+++ data_utils/image_funcs.py
@@ -3,27 +3,39 @@ import pandas as pd
 import os
 from scipy import misc
 import numpy as np
+import cv2
 
 class PreProcessor:
 
-    def __init__(self, input_dir):
-        self.input_dir = input_dir
 
-def resize_directory(input_dir, output_dir, size=(512, 512, 3)):
-    files = os.listdir(input_dir)
-    for f in files:
-        inpath = os.path.join(input_dir, f)
-        outpath = os.path.join(output_dir, f)
-        img = misc.imread(inpath)
-        img = misc.imresize(img, size=size)
-        misc.imsave(outpath, img)
+    def resize_directory(input_dir, output_dir, size=(512, 512, 3)):
+        files = os.listdir(input_dir)
+        for f in files:
+            inpath = os.path.join(input_dir, f)
+            outpath = os.path.join(output_dir, f)
+            img = misc.imread(inpath)
+            img = misc.imresize(img, size=size)
+            misc.imsave(outpath, img)
 
-def matrix_from_dir(directory, name='X_train'):
-    images = glob.glob(os.path.join(output_dir, "*"))
-    X = np.array([misc.imread(i) for i in images])
-    X = X.reshape(X.shape[0], 3, 512, 512)
-    np.save(os.path.join('..', 'data', 'X_train'), X)
+    def matrix_from_dir(directory, name='X_train'):
+        images = glob.glob(os.path.join(output_dir, "*"))
+        X = np.array([misc.imread(i) for i in images])
+        X = X.reshape(X.shape[0], 3, 512, 512)
+        np.save(os.path.join('..', 'data', 'X_train'), X)
 
+def smart_crop(im, threshold=30):
+    """
+    Detects and crops around eyes in each image
+    im = image as np array
+    threshold = min pixel value for binary-otsu thresholding
+
+    """
+    im = cv2.imread(im)
+    imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(imgray, threshold, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    contours = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    x, y, w, h = cv2.boundingRect(contours[0])
+    return im[y:y+h,x:x+w]
 
 
 
