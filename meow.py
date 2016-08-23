d211151b25182d2de1441f1d328669a290c976b7
Austin <jacobsonaustinj@gmail.com>
2016-08-23 18:19:43 -0400

    add standard learning rates, change optimizers, add new network architecture

--- meow.py
+++ meow.py
@@ -1,5 +1,21 @@
 from data_utils.data_funcs import get_labels
+# from keras.preprocessing.image import ImageDataGenerator
 import os
+import pandas as pd
 
 
-idx_label =  get_labels(os.path.abspath(os.path.join('E:', 'DR_Data', 'test')))
+# idx_label =  get_labels(os.path.abspath(os.path.join('E:', 'DR_Data', 'test')))
+#
+# test_datagen = ImageGenerator()
+#
+# test_generator = test_datagen.flow_from_directory(
+#         config['test_data_dir'],
+#         target_size=(512, 512),
+#         batch_size=5,
+#         class_mode=None,
+#         shuffle=False)
+#
+# print test_generator
+
+loss = pd.read_csv(os.path.join('training_history', 'resnetv1_2.csv'))
+print loss
--- train.py
+++ train.py
@@ -18,17 +18,28 @@ config = training_config
 nb_train_samples = 22481
 nb_validation_samples = 5620
 
+earlystop = EarlyStopping(monitor='val_loss', patience=2, verbose=1, mode='auto')
+checkpointer = ModelCheckpoint(
+            filepath=os.path.join('models',
+            'saved_models',
+            config['model_name']+'.hdf5'),
+            verbose=1,
+            save_best_only=False,
+            monitor='val_loss')
 
 #load model
 if config['continue_training'] == True:
     model = load_model(os.path.join('models', 'saved_models', config['model_name']+'.hdf5'))
+    if config['lower_lr'] == True:
+        current_lr =  model.optimizer.lr.get_value()
+        new_lr = current_lr / 10.
+        print "current_lr: ", current_lr
+        print "new_lr:", current_lr / 10.
+        model.optimizer.lr.set_value(new_lr)
 
 else:
-    model = resnet_v1()
+    model = resnet_v1(optimizer=config['optimizer'], loss=config['loss'])
 
-
-earlystop = EarlyStopping(monitor='val_loss', patience=4, verbose=1, mode='auto')
-checkpointer = ModelCheckpoint(filepath=os.path.join('models', 'saved_models', config['model_name']+'.hdf5'), verbose=1, save_best_only=False, monitor='val_loss')
 
 if config['prototype_model'] == True:
     # checks if model overfits one sample
@@ -41,18 +52,18 @@ if config['prototype_model'] == True:
             batch_size=config['batch_size'],
             nb_epoch=config['nb_epoch'],
             verbose=1,
-            callbacks=[checkpointer, earlystop],
+            callbacks=[checkpointer],
             class_weight=config['class_weight'])
 else:
-    # this is the augmentation configuration we will use for training
-    train_datagen = ImageDataGenerator(samplewise_center=True,
-                                        rotation_range=20,
-                                        width_shift_range=0.2,
-                                        height_shift_range=0.2,
-                                        zoom_range=0.2,
-                                        horizontal_flip=True)
+    train_datagen = ImageDataGenerator(
+                samplewise_center=True,
+                zoom_range=0.1,
+                horizontal_flip=True
+                )
 
-    test_datagen = ImageDataGenerator(samplewise_center=True)
+    test_datagen = ImageDataGenerator(
+                samplewise_center=True
+                )
 
     train_generator = train_datagen.flow_from_directory(
             config['train_data_dir'],
@@ -73,7 +84,6 @@ else:
             validation_data=validation_generator,
             nb_val_samples=nb_validation_samples,
             callbacks=[checkpointer, earlystop],
-            class_weight=config['class_weight'],
             nb_worker=4)
 
 # list all data in history
@@ -87,7 +97,7 @@ else:
 
 # summarize history for accuracy
 plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
+# plt.plot(history.history['val_acc'])
 plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
@@ -95,7 +105,7 @@ plt.legend(['train', 'test'], loc='upper left')
 plt.show()
 
 plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
+# plt.plot(history.history['val_loss'])
 plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
